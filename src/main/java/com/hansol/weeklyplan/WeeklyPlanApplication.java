f87255f4e1fac475d5e5631e9ab1895e24c5ae21
hjun-park <phj0860@gmail.com>
2022-11-16 15:54:38 +0900

    project init

---
+++ src/main/java/com/hansol/weeklyplan/WeeklyPlanApplication.java
@@ -0,0 +1,13 @@
+package com.hansol.weeklyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hansol/weeklyplan/WeeklyPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.weeklyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
